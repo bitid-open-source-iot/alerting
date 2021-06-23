8bdf27794ded0182e76e8d098cca1f5f85c62163
claytoncc <clayton@openiot.co.za>
2021-06-23 21:17:53 +0200

    updates to email alert & removed extra auth block

--- emails/emails.js
+++ emails/emails.js
@@ -5,30 +5,30 @@ const nodemailer = require('nodemailer');
 exports.alert = async (email, title, message, plain) => {
     const transporter = await nodemailer.createTransport(__settings.smtp);
 
-    // if (plain) {
-    //     const response = await transporter.sendMail({
-    //         'to': email,
-    //         'from': __settings.production ? __settings.from : __settings.smtp.auth.user,
-    //         'text': message,
-    //         'html': message,
-    //         'subject': title
-    //     });
+    if (plain) {
+        const response = await transporter.sendMail({
+            'to': email,
+            'from': __settings.production ? __settings.from : __settings.smtp.auth.user,
+            'text': message,
+            'html': message,
+            'subject': title
+        });
     
-    //     if (response.rejected.length > 0) {
-    //         return {
-    //             'ok': false,
-    //             'error': {
-    //                 'code': 401,
-    //                 'message': 'email rejected'
-    //             }
-    //         };
-    //     } else {
-    //         return {
-    //             'ok': true,
-    //             'result': {}
-    //         };
-    //     };
-    // } else {
+        if (response.rejected.length > 0) {
+            return {
+                'ok': false,
+                'error': {
+                    'code': 401,
+                    'message': 'email rejected'
+                }
+            };
+        } else {
+            return {
+                'ok': true,
+                'result': {}
+            };
+        };
+    } else {
         await transporter.use('compile', hbs({
             'viewEngine': {
                 'extName': '.hbs',
@@ -65,5 +65,5 @@ exports.alert = async (email, title, message, plain) => {
                 'result': {}
             };
         };
-    // }
+    }
 };
